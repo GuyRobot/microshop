1eb83f95baa40576775f3ee35089d5c73aaefdb9
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-05-11 09:14:11 +0700

    Config resilience circuit breaker

--- order-service/src/main/kotlin/com/guysrobot/orderservice/controller/OrderController.kt
+++ order-service/src/main/kotlin/com/guysrobot/orderservice/controller/OrderController.kt
@@ -2,12 +2,16 @@ package com.guysrobot.orderservice.controller
 
 import com.guysrobot.orderservice.dto.OrderRequest
 import com.guysrobot.orderservice.service.OrderService
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.lang.RuntimeException
+import java.util.concurrent.CompletableFuture
 
 @RestController
 @RequestMapping("/api/orders")
@@ -17,7 +21,21 @@ class OrderController(
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody orderRequest: OrderRequest) {
-        orderService.create(orderRequest)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    fun create(@RequestBody orderRequest: OrderRequest): CompletableFuture<String>? {
+        return CompletableFuture.supplyAsync {
+            orderService.create(orderRequest)
+            "Order successfully"
+        }
+    }
+
+    fun fallbackMethod(
+        @RequestBody orderRequest: OrderRequest,
+        runtimeException: RuntimeException
+    ): CompletableFuture<String>? {
+        return CompletableFuture.supplyAsync {
+            "Oops! Something went wrong, please try again later"
+        }
     }
 }
